950517c9e656a44fb0e4655510ddc263b5003553
Simón Almeida <xmithosx@Yahoo.com>
2020-06-06 23:07:40 +0200

    Client Basic Function Ready

--- InsuranceAPI/Controllers/PoliciesController.cs
+++ InsuranceAPI/Controllers/PoliciesController.cs
@@ -1,37 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using InsuranceAPI.Models;
+using System.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace InsuranceAPI.Controllers
 {
-    [Route("api/v{version}/policies")]
+    [Route("api/v{version}")]
     [ApiController]
     public class PoliciesController : ControllerBase
     {
         /// <summary>
-        /// Get the list of policies linked to a user name
-        /// </summary>
-        /// <param name="name">User Name</param>
-        /// <returns>Json list of values</returns>
-        [HttpGet("{name}")]
-        public IEnumerable<string> GetByName(string name)
-        {
-            return new string[] { "valueName" };
-        }
-
-        /// <summary>
         /// Get the user linked to a policy number - Assuming the police number is the Id
         /// </summary>
         /// <param name="id">police number</param>
+        /// <example>https://localhost:44305/api/v1/policiesbyid/64cceef9-3a01-49ae-a23b-3761b604800b</example>
         /// <returns>Json list of values</returns>
-        [HttpGet("{id:int}")]
-        public string GetById(int id)
+        [HttpGet("policiesbyid/{id}")]
+        public async Task<string> GetById(string id)
         {
-            return "valueID";
+            var tp = AltranDatos.GetPolicies();
+            await Task.WhenAll(tp);
+
+            var rawData = await tp;
+
+            var data = rawData.Policies.Where(x => x.Id == id);
+
+            return JsonConvert.SerializeObject(data);
+        }
+
+        /// <summary>
+        /// Get the list of policies linked to a user name
+        /// </summary>
+        /// <param name="name">User Name</param>
+        /// <example></example>
+        /// <returns>Json list of values</returns>
+        [HttpGet("policiesbyname/{name}")]
+        public async Task<string> GetByName(string name)
+        {
+            var tp = AltranDatos.GetPolicies();
+            await Task.WhenAll(tp);
+
+            var rawData = await tp;
+
+            var data = rawData.Policies;//.Where(x => x.Name == name);
+
+            return JsonConvert.SerializeObject(data);
         }
 
     }
--- InsuranceAPI/Models/Clients.cs
+++ InsuranceAPI/Models/Clients.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace InsuranceAPI.Models
 {
-    public class Clients
+    public class Cliente
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
 
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        [JsonPropertyName("email")]
         public string Email { get; set; }
 
-        public string role { get; set; }
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+    }
 
+    public class LClientes
+    {
+        [JsonPropertyName("clients")]
+        public IEnumerable<Cliente> Clients { get; set; }
     }
 }
