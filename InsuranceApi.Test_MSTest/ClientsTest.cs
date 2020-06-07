0c703715ba80c9d57d38b4e230a965c77abbf0ca
Simón Almeida <xmithosx@Yahoo.com>
2020-06-07 17:14:10 +0200

    Actualización De Colección Postman y finalización de UnitTest

--- InsuranceApi.Test_MSTest/ClientsTest.cs
+++ InsuranceApi.Test_MSTest/ClientsTest.cs
@@ -1,5 +1,6 @@
 using InsuranceAPI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,7 +20,6 @@ namespace InsuranceApi.Test_MSTest
         {
             //Arrange
             int expected = 194;
-            //string expeted2 = "a0ece5db-cd14-4f21-812f-966633e7be86";
 
             //Act
             var t1 = AltranDatos.GetClients();
@@ -28,10 +28,47 @@ namespace InsuranceApi.Test_MSTest
             var data = rawData.Clients;
 
             int actual = data.Count();
-            //string actual2 = data.ElementAt.ID;
 
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public async Task GetClients_GetingUserDataByIdAsync()
+        {
+            //Arrange
+            string expected = "[{\"Id\":\"a0ece5db-cd14-4f21-812f-966633e7be86\",\"Name\":\"Britney\",\"Email\":\"britneyblankenship@quotezart.com\",\"Role\":\"admin\"}]";
+
+            //Act
+            var t1 = AltranDatos.GetClients();
+            await Task.WhenAll(t1);
+            var rawData = await t1;
+            var data = rawData.Clients;
+
+            string actual = JsonConvert.SerializeObject(data.Where(x => x.Id == "a0ece5db-cd14-4f21-812f-966633e7be86"));
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task GetClients_GetingUserDataByNameAsync()
+        {
+            //Arrange
+            string expected = "[{\"Id\":\"a0ece5db-cd14-4f21-812f-966633e7be86\",\"Name\":\"Britney\",\"Email\":\"britneyblankenship@quotezart.com\",\"Role\":\"admin\"}]";
+
+            //Act
+            var t1 = AltranDatos.GetClients();
+            await Task.WhenAll(t1);
+            var rawData = await t1;
+            var data = rawData.Clients;
+
+            string actual = JsonConvert.SerializeObject(data.Where(x => x.Name == "Britney"));
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
 }
--- InsuranceApi.Test_MSTest/PoliciesTest.cs
+++ InsuranceApi.Test_MSTest/PoliciesTest.cs
@@ -1,5 +1,6 @@
 using InsuranceAPI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,7 +20,6 @@ namespace InsuranceApi.Test_MSTest
         {
             //Arrange
             int expected = 193;
-            //string expeted2 = "a0ece5db-cd14-4f21-812f-966633e7be86";
 
             //Act
             var t1 = AltranDatos.GetPolicies();
@@ -28,7 +28,24 @@ namespace InsuranceApi.Test_MSTest
             var data = rawData.Policies;
 
             int actual = data.Count();
-            //string actual2 = data.ElementAt.ID;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task GetPolicies_GetingPoliciByIDAsync()
+        {
+            //Arrange
+            string expected = "[{\"Date\":\"0001-01-01T00:00:00\",\"Id\":\"64cceef9-3a01-49ae-a23b-3761b604800b\",\"AmountInsured\":1825.89,\"Email\":\"inesblankenship@quotezart.com\",\"InceptionDate\":\"2016-06-01T03:33:32Z\",\"InstallmentPayment\":true,\"ClientId\":\"e8fd159b-57c4-4d36-9bd7-a59ca13057bb\"}]";
+
+            //Act
+            var t1 = AltranDatos.GetPolicies();
+            await Task.WhenAll(t1);
+            var rawData = await t1;
+            var data = rawData.Policies;
+
+            string actual = JsonConvert.SerializeObject(data.Where(x => x.Id == "64cceef9-3a01-49ae-a23b-3761b604800b"));
 
             //Assert
             Assert.AreEqual(expected, actual);
